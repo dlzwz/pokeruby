19cc35d10801ec4ba44263cf7db8337877b36cfb
dlzwz <da.lobato95@gmail.com>
2026-02-14 11:03:57 -0500

    Allow running indoors

--- src/bike.c
+++ src/bike.c
@@ -915,7 +915,7 @@ static u8 Bike_CheckCollisionTryAdvanceCollisionCount(struct ObjectEvent *object
 
 bool8 IsRunningDisallowed(u8 tile)
 {
-    if (IsRunningDisallowedByMetatile(tile) != FALSE || gMapHeader.mapType == MAP_TYPE_INDOOR)
+    if (IsRunningDisallowedByMetatile(tile) != FALSE)
         return TRUE;
     else
         return FALSE;
